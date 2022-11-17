0d2839d8e0b8591beef3ad72fa941168a4ddf4d0
Rana Kabeer <ranakb07@gmail.com>
2022-11-17 07:25:52 -0800

    created the fully responsive wgpt3 as well as features component

--- src/containers/Whatgpt3/Whatgpt3.jsx
+++ src/containers/Whatgpt3/Whatgpt3.jsx
@@ -1,7 +1,37 @@
 import React from 'react';
-import './whatFirstProject.css';
+import './whatgpt3.css';
+import Feature from '../../components/feature/Feature';
 const Whatgpt3 = () => {
-  return <div>WhatFirstProject</div>;
+  return (
+    <div className='gpt3__whatgpt3 section__margin' id='wgpt3'>
+      <div className='gpt3__whatgpt3-feature'>
+        <Feature
+          title='What is GPT-3'
+          text='We so opinion friends me message as delight. Whole front do of plate heard oh ought. His defective nor convinced residence own. Connection has put impossible own apartments boisterous. At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by.'
+        />
+      </div>
+      <div className='gpt3__whatgpt3-heading'>
+        <h1 className='gradient__text'>
+          The possibilites are beyound your imagination
+        </h1>
+        <p>Explore the Library</p>
+      </div>
+      <div classNa-me='gpt3__whatgpt3-container'>
+        <Feature
+          title='Chatbots'
+          text='We so opinion friends me message as delight. Whole front do of plate heard oh ought. '
+        />
+        <Feature
+          title='Knowledgebase'
+          text='At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by. As put impossible own apartments '
+        />
+        <Feature
+          title='Education'
+          text='At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by. As put impossible own apartments '
+        />
+      </div>
+    </div>
+  );
 };
 
 export default Whatgpt3;
